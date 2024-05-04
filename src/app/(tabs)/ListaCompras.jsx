b07ac698f4e5b67a2a01b4fb5763373fc8f3a22f
KinhoMilak <kinho_milak@hotmail.com>
2024-05-03 23:35:51 -0300

    adicionado lista de compras

--- src/app/(tabs)/ListaCompras.jsx
+++ src/app/(tabs)/ListaCompras.jsx
@@ -1,10 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
-import React, { useContext } from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import Header from "../../../components/Header";
-import Colors from "../../../constants/Colors";
+
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { CheckBox } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+import Header from '../../../components/Header';
+import Colors from '../../../constants/Colors';
+import React from "react";
+import { Tabs } from "expo-router";
+import Botoes from "../../../components/Botoes";
+import { Entypo } from "@expo/vector-icons";
 
 const ListaCompras = () => {
+  const [itens, setItens] = useState([
+    { nome: 'Arroz', id: '1', marcado: false },
+    { nome: 'Feijão', id: '2', marcado: false },
+    { nome: 'Açúcar', id: '3', marcado: false },
+   
+  ]);
+
+  const marcarItem = (id) => {
+    setItens(itens.map((item) => {
+      if (item.id === id) {
+        return { ...item, marcado: !item.marcado };
+      }
+      return item;
+    }));
+  };
+
   return (
     <LinearGradient
       colors={[Colors.grdienteInicio, Colors.gradienteFim]}
@@ -14,39 +35,65 @@ const ListaCompras = () => {
         <Header ativo={true} texto="Naum Marcirio" />
       </View>
       <View style={styles.containerTitulos}>
-        <Text style={styles.titulo}>Lista de Compras</Text>
-        <Text style={styles.subtitulo}>Sem Resultados...</Text>
+        <FlatList
+          data={itens}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <CheckBox
+                checked={item.marcado}
+                onPress={() => marcarItem(item.id)}
+                checkedColor='green'
+                uncheckedColor='white'
+              />
+              <Text style={styles.itemTexto}>{item.nome}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </LinearGradient>
   );
 };
+
 const styles = StyleSheet.create({
   containerGlobal: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
   container: {
     flex: 1,
-    width: "80%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-
   containerTitulos: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 8,
     gap: 12,
   },
   titulo: {
     color: Colors.brancoBase,
-    fontFamily: "KodChasanBold",
+    fontFamily: 'KodChasanBold',
     fontSize: 18,
   },
   subtitulo: {
     color: Colors.cinzaBase,
-    fontFamily: "KodChasanBold",
+    fontFamily: 'KodChasanBold',
     fontSize: 14,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 20,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+  },
+  itemTexto: {
+    marginLeft: 10,
   },
 });
 
